65b9b813363ce7ee2c19fc389123858ed1ee2c11
kostiuchenkoo <kostiuchenkoo@upmc.edu>
2023-07-08 23:40:44 +0300

    added proptypes to Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
     return (
@@ -10,6 +11,14 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
             <p>Positive Feedback: {positivePercentage}%</p>
         </div>
     );
+};
+
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
 };
 
 export default Statistics;
